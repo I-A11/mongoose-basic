11cbf1085b5e567751a5773df7edbca3b0bdab8c
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-01-30 15:36:55 +1100

    Start working with mongoose.

---
+++ index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/moviesApp")
+  .then(() => {
+    console.log("Connection open");
+  })
+  .catch((error) => {
+    console.log("A7a error");
+    console.log(error);
+  });
